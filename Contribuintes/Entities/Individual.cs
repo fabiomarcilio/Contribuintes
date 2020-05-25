c516e747302c4a6e82065165169c8c60e116eb87
Fabio Marcílio <fabiomarcilio@msn.com>
2020-05-25 12:17:34 -0300

    inicio da implementação do projeto contribuintes

---
+++ Contribuintes/Entities/Individual.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Contribuintes.Entities
+{
+    class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+
+        public Individual(string name, double anualIncome, double healthExpenditures) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double Tax()
+        {
+            return (AnualIncome * 0.25) - (HealthExpenditures * 0.50);
+        }
+
+    }
+}
